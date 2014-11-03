b62a01f9c92830432864dc36b6d7ab4077a51f34
Bob McElrath <bob_git@mcelrath.org>
2014-11-03 10:29:22 -0600

    Change port numbers 8333 -> 3764, 18333 -> 13764.

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -11,9 +11,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-    access = ServiceProxy("http://127.0.0.1:8332")
+    access = ServiceProxy("http://127.0.0.1:3763")
 else:
-    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:8332")
+    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:3763")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
