43844d203456bff65018d4fa2883b5a33a4c87bb
slick2 <carey.dayrit@gmail.com>
2015-01-24 06:09:59 +0800

    RBR

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -100,7 +100,7 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle *networkStyle, QWidget *parent) :
 {
     GUIUtil::restoreWindowGeometry("nWindow", QSize(850, 550), this);
 
-    QString windowTitle = tr("Bitcoin Core") + " - ";
+    QString windowTitle = tr("Ribbitcoin Core") + " - ";
 #ifdef ENABLE_WALLET
     /* if compiled with wallet support, -disablewallet can still disable the wallet */
     enableWallet = !GetBoolArg("-disablewallet", false);
